deb7509e9e80615fe4a4d58c03c6682cf689254e
shaxzodbe <sulaymanovshaxzod@gmail.com>
2024-01-04 18:37:25 +0000

    minor changes added

--- app/Color.php
+++ app/Color.php
@@ -3,8 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\App;
 
 class Color extends Model
 {
-    //
+    public function getTranslation($field = '', $lang = false){
+        $lang = $lang == false ? App::getLocale() : $lang;
+        $color_translation = $this->hasMany(ColorTranslation::class)->where('lang', $lang)->first();
+        return $color_translation != null ? $color_translation->$field : $this->$field;
+    }
+
+    public function color_translations(){
+        return $this->hasMany(ColorTranslation::class);
+    }
 }
