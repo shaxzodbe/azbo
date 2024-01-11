f9aef0029a9b1072348058a680a2bbb657686027
shaxzodbe <sulaymanovshaxzod@gmail.com>
2024-01-11 16:23:19 +0000

    UzumNasiya Integration

--- config/services.php
+++ config/services.php
@@ -64,12 +64,7 @@ return [
     ],
 
     'uzum_nasiya' => [
-        'env' => env('PAYTM_ENVIRONMENT'),
-        'merchant_id' => env('PAYTM_MERCHANT_ID'),
-        'merchant_key' => env('PAYTM_MERCHANT_KEY'),
-        'merchant_website' => env('PAYTM_MERCHANT_WEBSITE'),
-        'channel' => env('PAYTM_CHANNEL'),
-        'industry_type' => env('PAYTM_INDUSTRY_TYPE'),
+        'bearer_token' => env('UZUM_NASIYA_TOKEN'),
     ]
 
 ];
