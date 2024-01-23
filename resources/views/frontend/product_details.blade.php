010393def261b60a9738fcbbaffcefb8cab0d1ce
shaxzodbe <sulaymanovshaxzod@gmail.com>
2024-01-23 18:29:27 +0000

    minor changes

--- resources/views/frontend/product_details.blade.php
+++ resources/views/frontend/product_details.blade.php
@@ -1281,17 +1281,15 @@
 
             if (period == 3) {
                 $('#select-item-pay').html('' +
-                    '<div class="row">' +
-                        '<div class="mt-3">' +
-                            '<div class="collapse" id="collapseExample">' +
-                                '<div id="select-item-pay" style="display: inherit;">' +
-                                    '<button type="button" class="btn btn-soft-info fw-600 btn-block" onclick="payInInstallments()">' +
-                                        '<img src="https://azbo.uz/public/uploads/all/7Gv6oHzR6f617xdNiSJAjW1qNbwW2dgIaseDfcsM.png" class="h-50px" alt="">' +
-                                    '</button>' +
-                                    '<button type="button" class="btn btn-soft-info fw-600 btn-block" onclick="payInInstallments()">' +
-                                        '<img src="https://azbo.uz/public/uploads/all/rxCNoyRODbRR4P7VfZV82CtaRUC76857ZMfbkgrl.png" class="h-50px" alt="">' +
-                                    '</button>' +
-                                '</div>' +
+                    '<div class="mt-3">' +
+                        '<div class="collapse" id="collapseExample">' +
+                            '<div id="select-item-pay" style="display: inherit;">' +
+                                '<button type="button" class="btn btn-soft-info fw-600 btn-block" onclick="payInInstallments()">' +
+                                    '<img src="https://azbo.uz/public/uploads/all/7Gv6oHzR6f617xdNiSJAjW1qNbwW2dgIaseDfcsM.png" class="h-50px" alt="">' +
+                                '</button>' +
+                                '<button type="button" class="btn btn-soft-info fw-600 btn-block" onclick="payInInstallments()">' +
+                                    '<img src="https://azbo.uz/public/uploads/all/rxCNoyRODbRR4P7VfZV82CtaRUC76857ZMfbkgrl.png" class="h-50px" alt="">' +
+                                '</button>' +
                             '</div>' +
                     '</div>'
                 );
